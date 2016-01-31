ecaa05a12f919962cfd93c523bada71e6388e636
comtao <com_cai@sina.com>
2016-01-31 14:57:28 +0800

    优化

--- Book/src/main/java/com/lt/book/global/Configs.java
+++ Book/src/main/java/com/lt/book/global/Configs.java
@@ -5,4 +5,13 @@ package com.lt.book.global;
  */
 public class Configs {
 
+    /*
+    Application ID，SDK初始化必须用到此密钥
+    REST API Key，REST API请求中HTTP头部信息必须附带密钥之一
+    Secret Key，是SDK安全密钥，不可泄漏，在云端逻辑测试云端代码时需要用到
+    Master Key，超级权限Key。应用开发或调试的时候可以使用该密钥进行各种权限的操作*/
+
+    public static final String BOMB_APP_ID   = "b4a67e4733263699e5d60b3a07f49ca4";
+    public static final String BOMB_KEY      = "d5077bd0b2645b187d384d061257bb76";
+
 }
